fdd79a279c28073abac0935629189b79c322db08
vinci <vinci.zhang@gmail.com>
2011-02-26 18:53:52 +0800

    Rename Technology Radar to Technology Sky

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,26 +1,26 @@
-var radarBackgroundColor = "#ddd";
+var technologySkyBackgroundColor = "#ddd";
 
-var radarGridSpace = 10;
-var radarGridColor = "#eee";
+var technologySkyGridSpace = 10;
+var technologySkyGridColor = "#eee";
 
-var radarCoordinateColor = "#fff";
+var technologySkyCoordinateColor = "#fff";
 
-var radarRingFont =  "bold 12px sans-serif";
-var radarRingColor = "#fff";
+var technologySkyRingFont =  "bold 12px sans-serif";
+var technologySkyRingColor = "#fff";
 
-var radarQuadrantsFont = "bold 16px sans-serif";
-var radarQuadrantsColor = "#000";
-var radarQuadrantsLeftMargin = 30;
-var radarQuadrantsUpMargin = 30;
+var technologySkyQuadrantsFont = "bold 16px sans-serif";
+var technologySkyQuadrantsColor = "#000";
+var technologySkyQuadrantsLeftMargin = 30;
+var technologySkyQuadrantsUpMargin = 30;
 
-function Radar(radarId) {
-	var canvas = document.getElementById(radarId);
+function TechnologySky(TechnologySkyId) {
+	var canvas = document.getElementById(TechnologySkyId);
 	this.context = canvas.getContext("2d");
 	this.width = canvas.width;
 	this.height = canvas.height;
 }
 
-Radar.prototype.draw = function(){
+TechnologySky.prototype.draw = function(){
 	this.drawBackground();
 	this.drawGrid();
 	this.drawCoordinates();
@@ -28,38 +28,38 @@ Radar.prototype.draw = function(){
 	this.drawQuadrants();
 }
 
-Radar.prototype.drawBackground = function(){
-	this.context.fillStyle = radarBackgroundColor;
+TechnologySky.prototype.drawBackground = function(){
+	this.context.fillStyle = technologySkyBackgroundColor;
 	this.context.fillRect(0, 0, this.height, this.width);
 }
 
-Radar.prototype.drawGrid = function(){
-	for (var x = 0.5; x < this.width; x += radarGridSpace) {
+TechnologySky.prototype.drawGrid = function(){
+	for (var x = 0.5; x < this.width; x += technologySkyGridSpace) {
 	  this.context.moveTo(x, 0);
 	  this.context.lineTo(x, this.height);
 	}
 	
-	for (var y = 0.5; y < this.height; y += radarGridSpace) {
+	for (var y = 0.5; y < this.height; y += technologySkyGridSpace) {
 	  this.context.moveTo(0, y);
 	  this.context.lineTo(this.width, y);
 	}
-	this.context.strokeStyle = radarGridColor;
+	this.context.strokeStyle = technologySkyGridColor;
 	this.context.stroke();
 }
-Radar.prototype.drawCoordinates = function(){
+TechnologySky.prototype.drawCoordinates = function(){
 	this.context.beginPath();
 	this.context.moveTo(0, this.height/2);
 	this.context.lineTo(this.width, this.height/2);
 	this.context.moveTo(this.width/2, 0);
 	this.context.lineTo(this.width/2, this.height);
-	this.context.strokeStyle = radarCoordinateColor;
+	this.context.strokeStyle = technologySkyCoordinateColor;
 	this.context.stroke();
 }
 
-Radar.prototype.drawRings = function(){
+TechnologySky.prototype.drawRings = function(){
 	this.context.beginPath();
 	var radius = Math.min(this.height, this.width) / 2;
-	this.context.font = radarRingFont;
+	this.context.font = technologySkyRingFont;
 	this.context.textAlign = "center";
 	this.context.textBaseline = "middle";
 	
@@ -69,29 +69,29 @@ Radar.prototype.drawRings = function(){
 	this.drawRing((radius / 4) * 1.2, "Adopt");
 	
 	this.context.closePath();
-	this.context.strokeStyle = radarRingColor;
+	this.context.strokeStyle = technologySkyRingColor;
 	this.context.stroke();
 }
 
-Radar.prototype.drawRing = function(radius, text){
+TechnologySky.prototype.drawRing = function(radius, text){
 	this.context.arc(this.height/2, this.width/2, radius, 0, Math.PI * 2, false);
 	this.context.fillText(text, this.width/2, this.height/2 - radius);
 }
 
-Radar.prototype.drawQuadrants =  function(){
-	this.context.font = radarQuadrantsFont;
-	this.context.fillStyle = radarQuadrantsColor;
+TechnologySky.prototype.drawQuadrants =  function(){
+	this.context.font = technologySkyQuadrantsFont;
+	this.context.fillStyle = technologySkyQuadrantsColor;
 	this.context.textBaseline = "top";
 	this.context.textAlign = "left";
-	this.context.fillText("Techniques", radarQuadrantsLeftMargin, radarQuadrantsUpMargin);
+	this.context.fillText("Techniques", technologySkyQuadrantsLeftMargin, technologySkyQuadrantsUpMargin);
 	this.context.textBaseline = "top";
 	this.context.textAlign = "right";
-	this.context.fillText("Tools", this.width - radarQuadrantsLeftMargin, radarQuadrantsUpMargin);
+	this.context.fillText("Tools", this.width - technologySkyQuadrantsLeftMargin, technologySkyQuadrantsUpMargin);
 	
 	this.context.textBaseline = "bottom";
 	this.context.textAlign = "left";
-	this.context.fillText("Platforms", radarQuadrantsLeftMargin, this.height - radarQuadrantsUpMargin);
+	this.context.fillText("Platforms", technologySkyQuadrantsLeftMargin, this.height - technologySkyQuadrantsUpMargin);
 	this.context.textBaseline = "bottom";
 	this.context.textAlign = "right";
-	this.context.fillText("Languages", this.width - radarQuadrantsLeftMargin, this.height - radarQuadrantsUpMargin);
+	this.context.fillText("Languages", this.width - technologySkyQuadrantsLeftMargin, this.height - technologySkyQuadrantsUpMargin);
 }
