4bd745705c3e1559b1f123eb94d627d3bd210a10
vinci <vinci.zhang@gmail.com>
2011-02-26 18:19:40 +0800

    Could show background with grid and rings

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,58 +1,97 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var radarBackgroundColor = "#ddd";
 
-function drawBackground(context, height, width){
-	context.fillStyle = "#ddd";
-	context.fillRect(0, 0, height, width);
-				
-	for (var x = 0.5; x < width; x += 10) {
-	  context.moveTo(x, 0);
-	  context.lineTo(x, height);
+var radarGridSpace = 10;
+var radarGridColor = "#eee";
+
+var radarCoordinateColor = "#fff";
+
+var radarRingFont =  "bold 12px sans-serif";
+var radarRingColor = "#fff";
+
+var radarQuadrantsFont = "bold 16px sans-serif";
+var radarQuadrantsColor = "#000";
+var radarQuadrantsLeftMargin = 30;
+var radarQuadrantsUpMargin = 30;
+
+function Radar(radarId) {
+	var canvas = document.getElementById(radarId);
+	this.context = canvas.getContext("2d");
+	this.width = canvas.width;
+	this.height = canvas.height;
+}
+
+Radar.prototype.draw = function(){
+	this.drawBackground();
+	this.drawGrid();
+	this.drawCoordinates();
+	this.drawRings();
+	this.drawQuadrants();
+}
+
+Radar.prototype.drawBackground = function(){
+	this.context.fillStyle = radarBackgroundColor;
+	this.context.fillRect(0, 0, this.height, this.width);
+}
+
+Radar.prototype.drawGrid = function(){
+	for (var x = 0.5; x < this.width; x += radarGridSpace) {
+	  this.context.moveTo(x, 0);
+	  this.context.lineTo(x, this.height);
 	}
 	
-	for (var y = 0.5; y < height; y += 10) {
-	  context.moveTo(0, y);
-	  context.lineTo(width, y);
+	for (var y = 0.5; y < this.height; y += radarGridSpace) {
+	  this.context.moveTo(0, y);
+	  this.context.lineTo(this.width, y);
 	}
-	context.strokeStyle = "#eee";
-	context.stroke();
+	this.context.strokeStyle = radarGridColor;
+	this.context.stroke();
 }
-function drawCoordinates(context, height, width){	
-	context.beginPath();
-	context.moveTo(0, height/2);
-	context.lineTo(width, height/2);
-	context.moveTo(width/2, 0);
-	context.lineTo(width/2, height);
-	context.strokeStyle = "#fff";
-	context.stroke();
-}
-
-function drawRings(context, height, width){
-	context.beginPath();
-	var maxRadius = Math.min(height, width) / 2 - 10;
-	drawRing(context, height, width, maxRadius, "Hold");
-	drawRing(context, height, width, maxRadius - 10, "Hold");
-	context.closePath();
-	context.strokeStyle = "#fff";
-	context.stroke();
-}
-function drawRing(context, height, width, radius, text){
-	context.arc(height/2, width/2, radius, 0, Math.PI * 2, false);
+Radar.prototype.drawCoordinates = function(){
+	this.context.beginPath();
+	this.context.moveTo(0, this.height/2);
+	this.context.lineTo(this.width, this.height/2);
+	this.context.moveTo(this.width/2, 0);
+	this.context.lineTo(this.width/2, this.height);
+	this.context.strokeStyle = radarCoordinateColor;
+	this.context.stroke();
 }
 
-function drawQuadrants(context, height, width){
-	context.font = "bold 12px sans-serif";
-	context.fillStyle = "#000";
-	var upMargin = 5;
-	var leftMargin = 8;
-	context.textBaseline = "top";
-	context.fillText("Techniques", leftMargin, upMargin);
-	context.textAlign = "right";
-	context.fillText("Tools", width - leftMargin, upMargin);
+Radar.prototype.drawRings = function(){
+	this.context.beginPath();
+	var radius = Math.min(this.height, this.width) / 2;
+	this.context.font = radarRingFont;
+	this.context.textAlign = "center";
+	this.context.textBaseline = "middle";
 	
-	context.textBaseline = "bottom";
-	context.textAlign = "left";
-	context.fillText("Platforms", leftMargin, height - upMargin);
-	context.textAlign = "right";
-	context.fillText("Languages", width - leftMargin, height - upMargin);
+	this.drawRing((radius) * 0.95, "Hold");
+	this.drawRing((radius * 3 / 4) * 1, "Assess");
+	this.drawRing((radius / 2) * 1.1, "Trial");
+	this.drawRing((radius / 4) * 1.2, "Adopt");
+	
+	this.context.closePath();
+	this.context.strokeStyle = radarRingColor;
+	this.context.stroke();
+}
+
+Radar.prototype.drawRing = function(radius, text){
+	this.context.arc(this.height/2, this.width/2, radius, 0, Math.PI * 2, false);
+	this.context.fillText(text, this.width/2, this.height/2 - radius);
+}
+
+Radar.prototype.drawQuadrants =  function(){
+	this.context.font = radarQuadrantsFont;
+	this.context.fillStyle = radarQuadrantsColor;
+	this.context.textBaseline = "top";
+	this.context.textAlign = "left";
+	this.context.fillText("Techniques", radarQuadrantsLeftMargin, radarQuadrantsUpMargin);
+	this.context.textBaseline = "top";
+	this.context.textAlign = "right";
+	this.context.fillText("Tools", this.width - radarQuadrantsLeftMargin, radarQuadrantsUpMargin);
+	
+	this.context.textBaseline = "bottom";
+	this.context.textAlign = "left";
+	this.context.fillText("Platforms", radarQuadrantsLeftMargin, this.height - radarQuadrantsUpMargin);
+	this.context.textBaseline = "bottom";
+	this.context.textAlign = "right";
+	this.context.fillText("Languages", this.width - radarQuadrantsLeftMargin, this.height - radarQuadrantsUpMargin);
 }
